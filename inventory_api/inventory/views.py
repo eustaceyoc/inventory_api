eeea9cb730d38d1f7eba3a8a4e49413770c7bc02
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-02-26 00:30:44 +0000

    Implement InventoryItem CRUD API with user ownership and JWT authentication

--- inventory_api/inventory/views.py
+++ inventory_api/inventory/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import InventoryItem
+from .serializers import InventoryItemSerializer
 
-# Create your views here.
+class InventoryItemViewSet(viewsets.ModelViewSet):
+    serializer_class = InventoryItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Users see only their own items
+        return InventoryItem.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically assign the logged-in user as owner
+        serializer.save(user=self.request.user)
