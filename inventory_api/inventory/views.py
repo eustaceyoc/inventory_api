1fbd5be41336362084a4d86b6628c6eccd9ba3b3
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-02-26 01:01:15 +0000

    Add inventory/views.py to catch up with pushed files.

--- inventory_api/inventory/views.py
+++ inventory_api/inventory/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import InventoryItem
 from .serializers import InventoryItemSerializer
+from .permissions import IsOwner
+    
 
 class InventoryItemViewSet(viewsets.ModelViewSet):
     serializer_class = InventoryItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
-        # Users see only their own items
         return InventoryItem.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        # Automatically assign the logged-in user as owner
         serializer.save(user=self.request.user)
