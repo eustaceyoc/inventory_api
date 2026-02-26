8d4351f4bf784919ead7ed07b89ca10a8ff95950
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-02-26 00:38:33 +0000

    Add object-level permission to restrict inventory item management to owners only

--- inventory_api/inventory/permissions.py
+++ inventory_api/inventory/permissions.py
@@ -1,9 +1,5 @@
 from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit or delete it.
-    """
-
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
